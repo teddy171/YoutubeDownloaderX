125d081236e985330849ae94658e43dfa9920e1e
teddy171 <teddy171@qq.com>
2022-04-14 20:09:19 +0800

    Enable to get tasks with forms.

--- YoutubeDownloader/tests.py
+++ YoutubeDownloader/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
-
+import youtube_dl
 # Create your tests here.
+from videodownloader import VideoDownloader
+
+a = VideoDownloader("unity")
+try:
+    a.download_video()
+except youtube_dl.utils.DownloadError:
+    print("NA")
+    print(a.content)
--- YoutubeDownloader/views.py
+++ YoutubeDownloader/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from importlib.util import resolve_name
+from django.shortcuts import render, redirect
+
+from .models import Task
+from .forms import TaskForm
 
 def index(request):
-    """The home page for YoutubeDownload."""
+    """The home page for YoutubeDownloadX."""
     return render(request, 'YoutubeDownloader/index.html')
+def task(request):
+    """Show a single task"""
+    task = Task.objects.last
+    context = {'task': task}
+    return render(request, 'YoutubeDownloader/task.html', context)
+def new_task(request):
+    """Add a new topic."""
+    if request.method != 'POST':
+        # No data submitted; create a blank form.
+        form = TaskForm()
+    else:
+        # POST data submitted; process data.
+        form = TaskForm(data=request.POST)
+        if form.is_valid():
+            Task.objects.all().delete()
+            form.save()
+            return redirect('YoutubeDownloader:task')
+    # Display a blank or invalid form.
+    context = {'form': form}
+    return render(request, 'YoutubeDownloader/new_task.html', context)
