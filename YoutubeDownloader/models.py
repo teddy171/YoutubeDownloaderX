cb8f5fe587ce0ceb9b3f074fcb92eabe126d8936
teddy171 <teddy171@qq.com>
2022-04-15 11:39:34 +0800

    Enable to download video and send video by email.

--- YoutubeDownloader/models.py
+++ YoutubeDownloader/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     """The url or keyword the user is going to download."""
-    content = models.CharField(max_length=100)
+    content = models.TextField()
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self) -> str:
--- YoutubeDownloader/views.py
+++ YoutubeDownloader/views.py
@@ -1,6 +1,12 @@
+from importlib.resources import contents
 from importlib.util import resolve_name
 from django.shortcuts import render, redirect
+import youtube_dl
+from django.http import HttpResponse
+import datetime
 
+
+from .videoprocess import download_video
 from .models import Task
 from .forms import TaskForm
 
@@ -9,7 +15,7 @@ def index(request):
     return render(request, 'YoutubeDownloader/index.html')
 def task(request):
     """Show a single task"""
-    task = Task.objects.last
+    task = Task.objects.all()
     context = {'task': task}
     return render(request, 'YoutubeDownloader/task.html', context)
 def new_task(request):
@@ -21,9 +27,12 @@ def new_task(request):
         # POST data submitted; process data.
         form = TaskForm(data=request.POST)
         if form.is_valid():
-            Task.objects.all().delete()
             form.save()
             return redirect('YoutubeDownloader:task')
     # Display a blank or invalid form.
     context = {'form': form}
     return render(request, 'YoutubeDownloader/new_task.html', context)
+def download_task(request):
+    task = Task.objects.first()
+    download_video([str(task)])
+    return render(request, 'YoutubeDownloader/download_video.html')
